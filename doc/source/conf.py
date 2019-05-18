b632187218c103f4992502f91082e6ea06e99146
alexn11 <alexn11.gh@gmail.com>
2019-05-18 11:28:16 +0100

    changed version because of the new constructor

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ author = 'Alexandre De Zotti'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- mathsvg/__init__.py
+++ mathsvg/__init__.py
@@ -17,7 +17,7 @@ Type::
 for more details.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from mathsvg . mathsvg import SvgImage
 
